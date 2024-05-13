4ab4d11a74dee16b61b30b9bb214a000d47bfc79
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-12 20:44:25 -0700

    Update Simplify-the-number.js

--- Simplify-the-number.js
+++ Simplify-the-number.js
@@ -18,3 +18,15 @@ Note: input >= 0
 
 
 // Solution
+
+function simplify(number) {
+	return [ ...String(number) ]
+		.reduce((sum, num, index) => {
+			return num == 0
+				? (sum += '')
+				: String(number).length == index + 1
+					? (sum += `${num}+`)
+					: (sum += `${num}*${Math.pow(10, String(number).length - index - 1)}+`);
+		}, '')
+		.slice(0, -1);
+}
